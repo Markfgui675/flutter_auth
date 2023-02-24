c20dd26353372a0efd887b01d362e5d27c2035fb
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-02-24 17:12:13 -0300

    Conexão com o firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main(){
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
