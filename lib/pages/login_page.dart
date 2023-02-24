ea715707b593eaaed323304f519f18af281061a1
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-02-23 23:07:54 -0300

    Textfields

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,16 +1,68 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/components/my_textfield.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key});
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
+  final usernameController = TextEditingController();
+  final passwordContorller = TextEditingController();
 
-class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  const SizedBox(height: 50,),
+
+                  //logo
+                  Icon(Icons.lock, size: 100),
+                  const SizedBox(height: 50,),
+
+                  //welcome back, you've been missed!
+                  Text(
+                    "Welcome back you\'ve been missed",
+                    style: TextStyle(
+                        color: Colors.grey[700],
+                        fontSize: 16
+                    ),
+                  ),
+                  const SizedBox(height: 50,),
+
+
+                  //username
+                  MyTextField(
+                    hintText: 'E-mail',
+                    obscureText: false,
+                    controller: usernameController,
+                  ),
+                  const SizedBox(height: 10,),
+
+                  //pasword
+                  MyTextField(
+                    hintText: 'Password',
+                    obscureText: true,
+                    controller: passwordContorller,
+                  )
+
+                  //forgot password?
+
+                  //sign in button
+
+                  // or continue with
+
+                  //google + apple sign in button
+
+                  //not a member? register now
+                ],
+              ),
+            )
+        )
+
+    );
   }
 }
+
